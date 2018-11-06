e04d5185c65180151d9658976c3e4187b9ea0147
Jakub Sikora <jsikora@uxn14.ia.pw.edu.pl>
2018-11-06 15:55:49 +0100

    pliki z minixa:

---
+++ lab1/actual/soi1_lib.c
@@ -0,0 +1,10 @@
+#include "/usr/include/lib.h"
+#include "/usr/include/minix/type.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int add_nr_to_pid(int arg) {
+	message input;
+	input.m2_l2 = arg;
+	return 
+}
---
+++ lab1/actual/soi1_lib.h
@@ -0,0 +1,6 @@
+#ifndef __SOI1_H_
+#define __SOI1_H_
+
+int add_nr_to_pid(int arg);
+
+#endif
